a6cfeec58d9529fa789b76d712ca6754cb0bd146
KingSiong <kingsiong@163.com>
2022-04-15 18:38:00 +0800

    exp1&exp2

---
+++ Exp#2/network_awareness.py
@@ -0,0 +1,176 @@
+from ryu.base import app_manager
+from ryu.base.app_manager import lookup_service_brick
+from ryu.ofproto import ofproto_v1_3
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller import ofp_event
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet, arp
+from ryu.lib import hub
+from ryu.topology import event
+from ryu.topology.api import get_host, get_link, get_switch
+from ryu.topology.switches import LLDPPacket
+from ryu.base.app_manager import lookup_service_brick
+
+import networkx as nx
+import copy
+import time
+
+
+GET_TOPOLOGY_INTERVAL = 2
+SEND_ECHO_REQUEST_INTERVAL = .05
+GET_DELAY_INTERVAL = 2
+
+INF = 1e5
+
+class NetworkAwareness(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(NetworkAwareness, self).__init__(*args, **kwargs)
+        self.switch_info = {}  # dpid: datapath
+        self.link_info = {}  # (s1, s2): s1.port
+        self.port_link = {} # s1,port:s1,s2
+        self.port_info = {}  # dpid: (ports linked hosts)
+        self.topo_map = nx.Graph()
+        self.topo_thread = hub.spawn(self._get_topology)
+        self.echo_thread = hub.spawn(self._measure)
+        self.switches = None
+        self.lldp_delay = {}
+        self.echo_latency = {}
+
+        self.weight = 'delay'
+
+
+    def add_flow(self, datapath, priority, match, actions):
+        dp = datapath
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=dp, priority=priority, match=match, instructions=inst)
+        dp.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofp.OFPP_CONTROLLER, ofp.OFPCML_NO_BUFFER)]
+        self.add_flow(dp, 0, match, actions)
+
+    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def state_change_handler(self, ev):
+        dp = ev.datapath
+        dpid = dp.id
+
+        if ev.state == MAIN_DISPATCHER:
+            self.switch_info[dpid] = dp
+
+        if ev.state == DEAD_DISPATCHER:
+            del self.switch_info[dpid]
+
+    def clear_topology(self):
+        self.topo_map.clear()
+
+    def _get_topology(self):
+        _hosts, _switches, _links = None, None, None
+        while True:
+            hosts = get_host(self)
+            switches = get_switch(self)
+            links = get_link(self)
+
+            # update topo_map when topology change
+            if [str(x) for x in hosts] == _hosts and [str(x) for x in switches] == _switches and [str(x) for x in links] == _links:
+                continue
+            self.clear_topology()
+            _hosts, _switches, _links = [str(x) for x in hosts], [str(x) for x in switches], [str(x) for x in links]
+
+            for switch in switches:
+                self.port_info.setdefault(switch.dp.id, set())
+                # record all ports
+                for port in switch.ports:
+                    self.port_info[switch.dp.id].add(port.port_no)
+
+            for host in hosts:
+                # take one ipv4 address as host id
+                if host.ipv4:
+                    self.link_info[(host.port.dpid, host.ipv4[0])] = host.port.port_no
+                    self.topo_map.add_edge(host.ipv4[0], host.port.dpid, hop=1, delay=0, is_host=True)
+            for link in links:
+                # delete ports linked switches
+                self.port_info[link.src.dpid].discard(link.src.port_no)
+                self.port_info[link.dst.dpid].discard(link.dst.port_no)
+
+                # s1 -> s2: s1.port, s2 -> s1: s2.port
+                self.port_link[(link.src.dpid,link.src.port_no)]=(link.src.dpid, link.dst.dpid)
+                self.port_link[(link.dst.dpid,link.dst.port_no)] = (link.dst.dpid, link.src.dpid)
+
+                self.link_info[(link.src.dpid, link.dst.dpid)] = link.src.port_no
+                self.link_info[(link.dst.dpid, link.src.dpid)] = link.dst.port_no
+                delay = (self.lldp_delay.get((link.src.dpid, link.dst.dpid), INF) + \
+                    self.lldp_delay.get((link.dst.dpid, link.src.dpid), INF) - \
+                    self.echo_latency.get(link.src.dpid, 0) - \
+                    self.echo_latency.get(link.dst.dpid, 0)) / 2
+                if delay < 0:
+                    delay = 0
+                self.topo_map.add_edge(link.src.dpid, link.dst.dpid, hop=1, \
+                    delay=delay, is_host=False)
+
+            if self.weight == 'hop':
+                self.show_topo_map()
+            hub.sleep(GET_TOPOLOGY_INTERVAL)
+
+    def shortest_path(self, src, dst, weight='hop'):
+        try:
+            paths = list(nx.shortest_simple_paths(self.topo_map, src, dst, weight=weight))
+            return paths[0]
+        except:
+            self.logger.info('host not find/no path')
+
+    def show_topo_map(self):
+        self.logger.info('topo map:')
+        self.logger.info('{:^10s}  ->  {:^10s}'.format('node', 'node'))
+        for src, dst in self.topo_map.edges:
+            self.logger.info('{:^10s}      {:^10s}'.format(str(src), str(dst)))
+        self.logger.info('\n')
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        dpid = msg.datapath.id
+        try:
+            src_dpid, src_port_no = LLDPPacket.lldp_parse(msg.data)
+            if self.switches is None:
+                self.switches = lookup_service_brick('switches')
+            for port in self.switches.ports.keys():
+                if src_dpid == port.dpid and src_port_no == port.port_no:
+                    self.lldp_delay[(src_dpid, dpid)] = self.switches.ports[port].delay
+        except:
+            return
+
+    def _measure(self):
+        while True:
+            self._send_echo_request()
+            hub.sleep(SEND_ECHO_REQUEST_INTERVAL)
+
+    def _send_echo_request(self):
+        switches = None
+        switches = get_switch(self)
+        for switch in switches:
+            dp = switch.dp
+            parser = dp.ofproto_parser
+            echo_req = parser.OFPEchoRequest(dp, data='{:.10f}'.format(time.time()).encode("utf-8"))
+            dp.send_msg(echo_req)
+
+    @set_ev_cls(ofp_event.EventOFPEchoReply, MAIN_DISPATCHER)
+    def echo_reply_handler(self, ev):
+        try:
+            latency = time.time() - eval(ev.msg.data)
+            self.echo_latency[ev.msg.datapath.id] = latency
+        except:
+            return
+
---
+++ Exp#2/shortest_forward.py
@@ -0,0 +1,266 @@
+# ryu-manager shortest_forward.py --observe-links
+from dis import Instruction
+import math
+from tokenize import cookie_re
+from click import command
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER, HANDSHAKE_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet, arp, ipv4
+from ryu.controller import ofp_event
+from ryu.topology import event
+from ryu.topology.switches import LLDPPacket
+from ryu.base.app_manager import lookup_service_brick
+import sys
+from network_awareness import NetworkAwareness
+import networkx as nx
+ETHERNET = ethernet.ethernet.__name__
+ETHERNET_MULTICAST = "ff:ff:ff:ff:ff:ff"
+IDLE_TIMEOUT = 30
+HARD_TIMEOUT = 50
+ARP = arp.arp.__name__
+class ShortestForward(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _CONTEXTS = {
+        'network_awareness': NetworkAwareness
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(ShortestForward, self).__init__(*args, **kwargs)
+        self.network_awareness = kwargs['network_awareness']
+        self.weight = 'delay'
+        self.mac_to_port = {}
+        self.sw = {}
+        self.path = None
+
+    def add_flow(self, datapath, priority, match, actions, idle_timeout=0, hard_timeout=0):
+        dp = datapath
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=dp, priority=priority,
+            idle_timeout=idle_timeout,
+            hard_timeout=hard_timeout,
+            match=match, instructions=inst)
+        dp.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+
+        dpid = dp.id
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        arp_pkt = pkt.get_protocol(arp.arp)
+        ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
+
+        pkt_type = eth_pkt.ethertype
+
+        # layer 2 self-learning
+        dst_mac = eth_pkt.dst
+        src_mac = eth_pkt.src
+
+
+        if isinstance(arp_pkt, arp.arp):
+            self.handle_arp(msg, in_port, dst_mac,src_mac, pkt, pkt_type)
+
+        if isinstance(ipv4_pkt, ipv4.ipv4):
+            self.handle_ipv4(msg, ipv4_pkt.src, ipv4_pkt.dst, pkt_type)
+
+    def handle_arp(self, msg, in_port, dst, src, pkt, pkt_type):
+        #just handle loop here
+        #just like your code in exp1 mission2
+        dp = msg.datapath
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+        dpid = dp.id
+        self.mac_to_port.setdefault(dpid, {})
+        
+        self.sw.setdefault((dpid, src, dst), None)
+        if self.sw.get((dpid, src, dst)) == None:
+            self.sw[(dpid, src, dst)] = in_port
+        elif self.sw[(dpid, src, dst)] != in_port:
+            return
+        
+        self.mac_to_port[dpid][src] = in_port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofp.OFPP_FLOOD
+
+        actions = [dp.ofproto_parser.OFPActionOutput(out_port)]
+
+        if out_port != ofp.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_type=pkt_type)
+            self.add_flow(dp, 1, match, actions, IDLE_TIMEOUT, HARD_TIMEOUT)
+
+        data = None
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = dp.ofproto_parser.OFPPacketOut(
+            datapath = dp, buffer_id = msg.buffer_id, in_port = in_port,
+            actions = actions, data = data)
+        dp.send_msg(out)
+
+    def handle_ipv4(self, msg, src_ip, dst_ip, pkt_type):
+        parser = msg.datapath.ofproto_parser
+
+        dpid_path = self.network_awareness.shortest_path(src_ip, dst_ip, weight=self.weight)
+        if not dpid_path:
+            return
+
+        self.path = dpid_path
+
+        # get port path:  h1 -> in_port, s1, out_port -> h2
+        port_path = []
+        for i in range(1, len(dpid_path) - 1):
+            in_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i - 1])]
+            out_port = self.network_awareness.link_info[(dpid_path[i], dpid_path[i + 1])]
+            port_path.append((in_port, dpid_path[i], out_port))
+        self.show_path(src_ip, dst_ip, port_path)
+        # calc path delay
+
+
+        # send flow mod
+        for node in port_path:
+            in_port, dpid, out_port = node
+            self.send_flow_mod(parser, dpid, pkt_type, src_ip, dst_ip, in_port, out_port)
+            self.send_flow_mod(parser, dpid, pkt_type, dst_ip, src_ip, out_port, in_port)
+
+        # send packet_out
+        _, dpid, out_port = port_path[-1]
+        dp = self.network_awareness.switch_info[dpid]
+        actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(
+            datapath=dp, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=msg.data)
+        dp.send_msg(out)
+
+    def send_flow_mod(self, parser, dpid, pkt_type, src_ip, dst_ip, in_port, out_port):
+        dp = self.network_awareness.switch_info[dpid]
+        match = parser.OFPMatch(
+            in_port=in_port, eth_type=pkt_type, ipv4_src=src_ip, ipv4_dst=dst_ip)
+        actions = [parser.OFPActionOutput(out_port)]
+        self.add_flow(dp, 1, match, actions, IDLE_TIMEOUT, HARD_TIMEOUT)
+
+    def show_path(self, src, dst, port_path):
+        self.logger.info('path: {} -> {}'.format(src, dst))
+        path = src + ' -> '
+        for node in port_path:
+            path += '{}:s{}:{}'.format(*node) + ' -> '
+        path += dst
+        self.logger.info(path)
+    
+    def delete_flow(self, dp, idle_timeout=0, hard_timeout=0):
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+
+        for dst in self.mac_to_port[dp.id].keys():
+            match = parser.OFPMatch(eth_dst=dst)
+            mod = parser.OFPFlowMod(
+                datapath=dp, cookie=0, cookie_mask=0, table_id=0, command=ofp.OFPFC_DELETE,
+                idle_timeout=idle_timeout, hard_timeout=hard_timeout, priority=1, buffer_id=ofp.OFP_NO_BUFFER, 
+                out_port=ofp.OFPP_ANY, out_group=ofp.OFPG_ANY, flags=ofp.OFPFF_SEND_FLOW_REM, 
+                match=match, 
+                instructions=None)
+            dp.send_msg(mod)
+
+    def del_flow(self, datapath, priority, match, out_port, idle_timeout=0, hard_timeout=0):
+        dp = datapath
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+
+        mod = parser.OFPFlowMod(datapath=dp, cookie=0, cookie_mask=0, table_id=0,
+                command=ofp.OFPFC_DELETE,
+                idle_timeout=idle_timeout, hard_timeout=hard_timeout,
+                priority=priority,
+                buffer_id=ofp.OFP_NO_BUFFER,
+                out_port=out_port, out_group=ofp.OFPG_ANY, flags=ofp.OFPFF_SEND_FLOW_REM,
+                match=match,
+                instructions=None)
+        dp.send_msg(mod)
+
+    def sup_del_flow(self, dp, priority=1):
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+        match = parser.OFPMatch(eth_type=0x0800)
+        mod = parser.OFPFlowMod(datapath=dp, cookie=0, cookie_mask=0, table_id=0,
+                command=ofp.OFPFC_DELETE,
+                idle_timeout=0, hard_timeout=0,
+                priority=priority,
+                buffer_id=ofp.OFP_NO_BUFFER,
+                out_port=ofp.OFPP_ANY, out_group=ofp.OFPG_ANY, flags=ofp.OFPFF_SEND_FLOW_REM,
+                match=match,
+                instructions=None)
+        dp.send_msg(mod)
+        match = parser.OFPMatch(eth_type=0x0806)
+        mod = parser.OFPFlowMod(datapath=dp, cookie=0, cookie_mask=0, table_id=0,
+                command=ofp.OFPFC_DELETE,
+                idle_timeout=0, hard_timeout=0,
+                priority=priority,
+                buffer_id=ofp.OFP_NO_BUFFER,
+                out_port=ofp.OFPP_ANY, out_group=ofp.OFPG_ANY, flags=ofp.OFPFF_SEND_FLOW_REM,
+                match=match,
+                instructions=None)
+        dp.send_msg(mod)
+
+    # @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    # def port_status_handler(self, ev):
+    #     msg = ev.msg
+    #     dp = msg.datapath
+    #     ofp = dp.ofproto
+    #     parser = dp.ofproto_parser
+
+    #     if msg.reason == ofp.OFPPR_MODIFY:
+    #         reason = 'MODIFY'
+    #         if msg.desc.state == ofp.OFPPS_LINK_DOWN or msg.desc.state == ofp.OFPPS_LIVE:
+    #             self.sw.clear()
+    #             if dp.id in self.path:
+    #                 # arp flow_tables
+    #                 for i in range(1, len(self.path) - 1):
+    #                     now_dpid = self.path[i]
+    #                     now_dp = self.network_awareness.switch_info[now_dpid]
+    #                     if now_dpid in self.mac_to_port:
+    #                         self.delete_flow(now_dp)
+    #                         del self.mac_to_port[now_dpid]
+    #                 # ipv4 flow_tables
+    #                 for i in range(1, len(self.path) - 1):
+    #                     in_port = self.network_awareness.link_info[(self.path[i], self.path[i - 1])]
+    #                     out_port = self.network_awareness.link_info[(self.path[i], self.path[i + 1])]
+    #                     dpid = self.path[i]
+    #                     dp = self.network_awareness.switch_info[dpid]
+    #                     match = parser.OFPMatch(in_port=in_port, eth_type=0x0800, ipv4_src=self.path[0], ipv4_dst=self.path[-1])
+    #                     self.del_flow(dp, 1, match, out_port, IDLE_TIMEOUT, HARD_TIMEOUT)
+    #                     match = parser.OFPMatch(in_port=out_port, eth_type=0x0800, ipv4_src=self.path[-1], ipv4_dst=self.path[0])
+    #                     self.del_flow(dp, 1, match, in_port, IDLE_TIMEOUT, HARD_TIMEOUT)
+    #             self.logger.info('OFPPortStatus received: reason=%s, desc=%s',
+    #                     reason, msg.desc)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
+    def port_status_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        parser = dp.ofproto_parser
+
+        if msg.reason == ofp.OFPPR_MODIFY:
+            reason = 'MODIFY'
+            if msg.desc.state == ofp.OFPPS_LINK_DOWN or msg.desc.state == ofp.OFPPS_LIVE:
+                self.sw.clear()
+                self.mac_to_port.clear()
+                for dp in self.network_awareness.switch_info.values():
+                    self.sup_del_flow(dp)
+
+                self.logger.info('OFPPortStatus received: reason=%s, desc=%s',
+                        reason, msg.desc)
